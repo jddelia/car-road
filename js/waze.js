ad03909810c3a390e3c7e43538123337c9826056
jddelia <jddelia123@gmail.com>
2019-03-18 15:47:52 -0400

    Update animation to transform

--- js/waze.js
+++ js/waze.js
@@ -1,10 +1,10 @@
 let car = document.getElementById('car');
 let carPosition = (window.innerHeight / 2.5) + window.pageYOffset;
-car.setAttribute("style", `top: ${carPosition}px;`);
+car.setAttribute("style", `transform: translateY(${carPosition}px);`);
 
 function driveCar() {
   carPosition = (window.innerHeight / 2.5) + window.pageYOffset;
-  car.setAttribute("style", `top: ${carPosition}px;`);
+  car.setAttribute("style", `transform: translateY(${carPosition}px);`);
 }
 
 window.addEventListener('scroll', driveCar);
